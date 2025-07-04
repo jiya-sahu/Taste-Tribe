f50dc08d7d327ce56bdff19072b65f8cab6baa7f
jiya-sahu <jiyasahu135@gmail.com>
2025-07-04 23:38:38 +0530

    added filter food functionality according to category

--- src/components/FoodItems.jsx
+++ src/components/FoodItems.jsx
@@ -1,28 +1,34 @@
-import React from 'react'
 import FoodCard from './FoodCard'
 import FoodData from '../data/fooddata.js'
 import toast, { Toaster } from "react-hot-toast";
+import { useSelector } from 'react-redux';
 const FoodItems = () => {
   const handleToast = (name) => toast.success(`Added ${name} `);
+  const selectedCategory = useSelector((state) => state.category.category);
   return (
     <>
        <Toaster position="top-center" reverseOrder={false} />
-      <div className='flex flex-row  flex-wrap sm:[flex-col] gap-10 justify-center lg:justify-start mx-6 my-10 '>{
-        FoodData.map((food)=>{
-          return (<FoodCard 
-          key={food.id}
-          id={food.id}
-          name={food.name}
-          desc={food.desc}
-          img={food.img}
-          category={food.category}
-          price={food.price}
-          rating={food.rating}
-          handleToast={handleToast}/>
-        )
-        })
-      }
-      
+      <div className='flex flex-row  flex-wrap sm:[flex-col] gap-10 justify-center lg:justify-start mx-6 my-10 '>
+        {
+          FoodData.filter((food) => {
+            if(selectedCategory === "All") {
+              return food;
+            }else{
+              return food.category === selectedCategory;
+            }
+          })
+          .map((food) => (
+            <FoodCard
+              key={food.id}
+              id={food.id}
+              name={food.name}
+              desc={food.desc}
+              img={food.img}
+              price={food.price}
+              rating={food.rating}
+              handleToast={handleToast}
+            />
+          ))}
     </div>
     </>
   )
